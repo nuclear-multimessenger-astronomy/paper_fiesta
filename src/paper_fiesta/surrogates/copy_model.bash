335c7e92046d06b5943ecaff2958bcd94ec06afb
haukekoehn <hauke.koehn@uni-potsdam.de>
2025-03-25 14:45:33 +0100

    afgpy tophat model

---
+++ src/paper_fiesta/surrogates/copy_model.bash
@@ -0,0 +1,6 @@
+cp /work/koehn1/fiesta/fiesta/surrogates/GRB/afgpy_gaussian_CVAE/model/* ./afgpy_gaussian_CVAE/
+cp /work/koehn1/fiesta/fiesta/surrogates/GRB/afgpy_gaussian_MLP/model/* ./afgpy_gaussian_MLP/
+cp /work/koehn1/fiesta/fiesta/surrogates/GRB/afgpy_tophat_CVAE/model/* ./afgpy_tophat_CVAE/
+cp /work/koehn1/fiesta/fiesta/surrogates/GRB/afgpy_tophat_MLP/model/* ./afgpy_tophat_MLP/
+cp /work/koehn1/fiesta/fiesta/surrogates/GRB/pbag_gaussian_CVAE/model/* ./pbag_gaussian_CVAE/
+cp /work/koehn1/fiesta/fiesta/surrogates/GRB/pbag_gaussian_MLP/model/* ./pbag_gaussian_MLP/
