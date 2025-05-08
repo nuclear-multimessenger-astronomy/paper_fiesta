23ac4fbe5ad2f8c39b4ac58ba64f2397609be24e
haukekoehn <hkoehn@aya.gw.physik.uni-potsdam.de>
2025-05-08 14:59:25 +0200

    Scaling plot

--- src/paper_fiesta/performance/scaling_pbag/performance_plot.py
+++ src/paper_fiesta/performance/scaling_pbag/performance_plot.py
@@ -10,20 +10,41 @@ plt.rcParams.update({"text.usetex" : True,
         "font.serif" : ["Computer Modern Serif"]})
 
 
-fig, ax = plt.subplots(1, 1, figsize=(6, 4.8))
+def read_timing_file(filename):
+    data = np.loadtxt(filename)
+    D = np.sort(np.unique(data[:,0]))
+    
+    total_time = []
+    compile_time = []
 
-D, total_time, compile_time = np.loadtxt("./outdir/Quadro_RTX_6000_timing.txt", unpack=True)
-ax.plot(D, total_time, linestyle="dashed", color="grey", alpha=0.5)
-ax.scatter(D, total_time, c="blue", marker="s", label="NVIDIA RTX 6000")
+    for d in D:
+        mask = data[:,0]==d
+        total_time.append(np.mean(data[mask, 1]))
+        compile_time.append(np.mean(data[mask,2]))
+
+    return D, np.array(total_time), np.array(compile_time)
+
+
+fig, ax = plt.subplots(1, 1, figsize=(6, 4.8))
+fig.subplots_adjust(bottom=0.12, left=0.1, top=0.98, right=0.98)
+
+D, total_time, compile_time = read_timing_file("./outdir/RTX_6000_timing.txt")
+ax.plot(D, total_time-compile_time, linestyle="dashed", color="grey", alpha=0.5)
+ax.scatter(D, total_time-compile_time, c="blue", marker="s", label="NVIDIA RTX 6000")
+
+D, total_time, compile_time = read_timing_file("./outdir/H100_timing.txt")
+ax.plot(D, total_time-compile_time, linestyle="dashed", color="grey", alpha=0.5)
+ax.scatter(D, total_time-compile_time, c="red", marker="s", label="NVIDIA H100")
 
 
 
 ax.set_xlabel("Number of sampling parameters", fontsize=15)
 ax.set_ylabel("Runtime in s", fontsize=15)
-ax.set(xscale="log", yscale="log")
+#ax.set(xscale="log", yscale="log")
 
-ax.set_xticks([10, 20, 40], [10, 20, 40], fontsize=12)
-ax.set_yticks([70, 80, 90, 100], [70, 80, 90, 100], fontsize=12)
+ax.set_xticks([10, 20, 40, 80], [10, 20, 40, 80], fontsize=12)
+#ax.set_yticks([50, 70, 100, 140], [50, 70, 100, 140], fontsize=12)'
+ax.set_yticks(range(50, 250, 50), range(50, 250, 50), fontsize=12)
 
 ax.set_xticks([], [], fontsize=12, minor=True)
 ax.set_yticks([], [], fontsize=12, minor=True)
